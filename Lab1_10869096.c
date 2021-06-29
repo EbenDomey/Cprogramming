bc3f3f033393d2e06f3be83ff58339c398f3d6c8
BlackExcalibur <domeyeben4@gmail.com>
2021-06-29 16:54:32 +0000

    Lab1-Uni

--- Lab1_10869096.c
+++ Lab1_10869096.c
@@ -1,5 +1,11 @@
 #include<stdio.h>
 int main(){
     printf("Helloworld\n");
+    int a;//declaration
+    int b=2;
+    a = 3;//initialisation. This stores the value of 2 to the variable a until its changed
+    int c;
+    c = a + b;
+    printf("The sum of %d and %d is %d\n",a,b,c);
     return 0;
 }
