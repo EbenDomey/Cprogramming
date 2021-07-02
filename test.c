a2d9ddf9b41d8eb6cc3bb82ca6dd2eb5319eaa66
BlackExcalibur <domeyeben4@gmail.com>
2021-07-02 20:13:27 +0000

    C_Test

---
+++ test.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main(){
+    float Basic,DA,HRA,Gs;
+    Basic = 500.0;
+    DA=0.4 *Basic;
+    HRA=0.2 *Basic;
+    Gs = DA+HRA+Basic;
+    printf("%f\n",Gs);
+    float c=(float) 98/10;
+    printf("%f",c);
+
+}
