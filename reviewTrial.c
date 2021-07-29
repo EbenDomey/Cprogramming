2148cee920f58db38d6d1226a18fd643c4f99265
BlackExcalibur <domeyeben4@gmail.com>
2021-07-29 18:28:59 +0000

    The answer for the n to -n rash

--- reviewTrial.c
+++ reviewTrial.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
+    int i,j;
+    printf("Input your value for n\n");
+    scanf("%d", &i);
+    j=-i;
+    for(;i>=j;i--){
+        printf("%d\n",i);
+    }
 
 }
