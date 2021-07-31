524ad2fc9194f797c7925ecdc4adec620baa3ea2
BlackExcalibur <domeyeben4@gmail.com>
2021-07-31 21:38:31 +0000

    remove comment notation block by block before running

--- reviewTrial.c
+++ reviewTrial.c
@@ -1,12 +1,112 @@
 #include<stdio.h>
 #include<math.h>
 int main(){
-    int i,j;
-    printf("Input your value for n\n");
-    scanf("%d", &i);
-    j=-i;
-    for(;i>=j;i--){
-        printf("%d\n",i);
-    }
+int i,j,k;
+//for you readers please remove comments for each code block before running else ull get error
+/*printf("Please input your four digit numbers\n");
+scanf("%d",&i);
+if(i%2==0&& i/2==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%3==0&& i/3==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%4==0&& i/4==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%5==0&& i/5==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%6==0&& i/6==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%7==0&& i/7==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%8==0&& i/8==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}
+else if(i%9==0&& i/9==1111){
+    printf("The four digits consist of the same kind of numbers\n");
+}else{
+    printf("The four digit numbers are different\n");
+}*/
 
+/*
+printf("Input the year please\n");
+scanf("%d",&i);
+if(i>0){
+    if(i%4==0){
+        printf("This year has 366 days and february has 29 days\n");
+    }else{
+        printf("This year has 365 days and february has 28 days\n");
+    }
+}else{
+    printf("This is not a valid year\n");
+}
+*/
+
+/*
+printf("First number: ");
+scanf("%d",&i);
+printf("Second number: ");
+scanf("%d",&j);
+k=i*j;
+if(k<0){
+    printf("It's result is negative\n");
+}else if(k>0){
+    if(k%2==0){
+        printf("This is an even number\n");
+    }else {
+        printf("This is an odd number\n");
+    }
+}*/
+
+/*
+for(i=1;i<5;i++){
+    switch(i){
+        case 1:
+        printf("*\n");
+        break;
+        case 2:
+        printf("**\n");
+        break;
+        case 3:
+        printf("***\n");
+        break;
+        case 4:
+        printf("****\n");
+        break;
+    }
+}*/
+
+/*
+printf("Input a number\n");
+scanf("%d",&i);
+if(i%2==0 && i%5==0){
+    printf("This number is divisible by 2 and 5\n");
+}else{
+    printf("This number is not divisible by 2 and 5\n");
+}*/
+
+/*
+printf("start: ");
+scanf("%d", &i);
+printf("Stop: ");
+scanf("%d", &j);
+for(k=0;i<j;i++){
+if(i%2!=0){
+    continue;
+}
+k=k+i;
+printf("%d\n", k);
+}*/
+
+/*
+printf("Input a number\n");
+scanf("%d",&i);
+k=-i;
+for(;i>=k;i--){
+    printf("%d\n",i);
+}*/
 }
