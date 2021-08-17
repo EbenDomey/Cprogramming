a47f63b53957cc4e5e4601d6c6d637411a68dc43
BlackExcalibur <domeyeben4@gmail.com>
2021-08-17 21:50:36 +0000

    Array stuff

--- arrays.c
+++ arrays.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 int main(){
-    int i,j,temp;
+    int i,j,temp,largest,secLargest;
     int a[10]={ 10, 9, 7, 101, 23, 44, 12, 78, 34, 23};
-    for(i=0;i<10;i++){
+    /*for(i=0;i<10;i++){
         for(j=i+1;j<10;j++){
             if(a[j]>a[i]){
                 temp=a[i];
@@ -18,5 +18,17 @@ int main(){
     for(j=0;j<10;j++){
         printf("%d,",a[j]);//for sorted in descending;
     }
-    printf("\n");
+    printf("\n");*/
+    largest=a[1];
+    secLargest=a[2];
+    for(i=0;i<10;i++){
+        if(a[i]>largest){
+            largest=a[i];
+            secLargest=largest;
+    }
+    else if(a[i]>secLargest &&a[i]!=largest){
+        secLargest=a[i];
+}
+}
+printf("Largest is %d\nSecond largest is %d\n",largest,secLargest);
 }
