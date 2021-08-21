bb1062731e0275c29601fdfdc090431a2a2743b6
BlackExcalibur <domeyeben4@gmail.com>
2021-08-21 09:20:08 +0000

    2dArray

--- 2dArray.c
+++ 2dArray.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 int main(){
-    int i,j;
+    int i=0,j=0;
     /*int arr[4][3]={{1,2,3},{2,3,4},{3,4,5},{4,5,6}};
     for(i=0;i<4;i++){
         for(j=0;j<3;j++){
             printf("%d\t",arr[i][j]);
         }
         printf("\n");
-    }*/
+    }
     //let's create a matrix shall we..
     int matA[3][2],matB[3][2];
     printf("MatA Input:\n");
@@ -40,5 +40,18 @@ int main(){
         }
         printf("\n");
     }
+    printf("Total matrice is:\n");
+    for(i=0;i<2;i++){
+        for(j=0;j<2;j++){
+            printf("%d\t",matA[i][j]+matB[i][j]);
+        }
+        printf("\n");
+    }*
+    for(i=0;i<=5;i++){
+        for(j=0;j<=i;j++){
+            printf("*");
+        }
+        printf("\n");
+    }*/
     return 0;
 }
